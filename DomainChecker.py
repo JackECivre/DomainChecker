e67408d0c9e5d48ac52cc7da1df0822d42cac707
jackecivre <jackecivre@gmail.com>
2021-12-28 17:02:29 +0200

    checker functional - working on timer and alert system

--- DomainChecker.py
+++ DomainChecker.py
@@ -2,29 +2,50 @@ import requests
 from datetime import datetime
 import time
 from plyer import notification
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def domain_checker(domain):
-    print("\nPlease write the domain adress to be checked\n")
+def hourly_checker(function):
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(function, 'interval', hours=6)
+    scheduler.start()
+
+
+def domain_checker():
     domain = []
-    pre_domain = input()
-    domain = pre_domain.split()
-    print("domain is " + str(domain))
-
     try:
-        for url in domain:
+        file1 = open("D:\Gissel\dev\DomainChecker\domains.TXT", 'r')
+        lines = file1.readlines()
+        print("lines are: " + str(lines))
+    except Exception as error:
+        print("Text file error: \n" + str(error))
+
+    for line in lines:
+        try:
+            new_line = line[:-1]
+            domain.append(new_line)
+        except Exception as error:
+            print("line iteration error: \n" + str(error))
+
+    # pre_domain = input()
+    # domain = pre_domain.split()
+    # print("Domains are: " + str(domain))
+
+    for url in domain:
+        try:
             print("URL is " + str(url))
             result = requests.get(url)
             print(result)
-            if result.status_code == 200:
-                print("All Good")
-                continue
-            else:
+            if result.status_code != 200:
                 print("PROBLEM WITH DOMAIN " + str(url))
-                continue
-    except Exception as error:
-        print("Problem checking domain " + str(error))
+            else:
+                print("All Good")
+
+        except Exception as error:
+            print("PROBLEM WITH DOMAIN: " + str(url))
+            print("Problem: " + str(error))
 
 
-domain_checker()
+hourly_checker(domain_checker())
 

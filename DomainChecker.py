155ff9babf464599a6944c3b6c4973f103646799
jackecivre <jackecivre@gmail.com>
2022-01-06 13:50:20 +0200

    scheduler changed into simpler code. restructuring done. - Alert will be created

--- DomainChecker.py
+++ DomainChecker.py
@@ -1,15 +1,19 @@
 import requests
-from datetime import datetime
 import time
 from plyer import notification
-from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 def hourly_checker(function):
-
-    scheduler = BlockingScheduler()
-    scheduler.add_job(function, 'interval', hours=6)
-    scheduler.start()
+    try:
+        while True:
+            function
+            print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+            print(" + Check Complete\n + 1 hour to check.")
+            time.sleep(1800)
+            print(" + 30 minutes to check.")
+            time.sleep(1800)
+    except Exception as error:
+        print(" - Scheduler error: " + str(error))
 
 
 def domain_checker():
@@ -17,35 +21,30 @@ def domain_checker():
     try:
         file1 = open("D:\Gissel\dev\DomainChecker\domains.TXT", 'r')
         lines = file1.readlines()
-        print("lines are: " + str(lines))
+        print(" + Lines are: " + str(lines))
     except Exception as error:
-        print("Text file error: \n" + str(error))
+        print(" - Text file error: \n" + str(error))
 
     for line in lines:
         try:
             new_line = line[:-1]
             domain.append(new_line)
         except Exception as error:
-            print("line iteration error: \n" + str(error))
-
-    # pre_domain = input()
-    # domain = pre_domain.split()
-    # print("Domains are: " + str(domain))
+            print(" - Line iteration error: \n" + str(error))
 
     for url in domain:
         try:
-            print("URL is " + str(url))
+            print(" + Checking Url: " + str(url))
             result = requests.get(url)
             print(result)
             if result.status_code != 200:
-                print("PROBLEM WITH DOMAIN " + str(url))
+                print(" ! PROBLEM WITH DOMAIN " + str(url))
             else:
-                print("All Good")
+                print(" + All Good")
 
         except Exception as error:
-            print("PROBLEM WITH DOMAIN: " + str(url))
-            print("Problem: " + str(error))
+            print(" - Problem on Domain: " + str(url))
+            print(" - Problem: " + str(error))
 
 
 hourly_checker(domain_checker())
-
